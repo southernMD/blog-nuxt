2d51f2611ed1356adc45d23d3926198dfb72e932
southernMD <2483723241@qq.com>
2022-11-21 18:03:33 +0800

    seo

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
     <Head>
         <Title>南山有壶酒</Title>
+        <Meta name="author" content="southernMD"></Meta>
+        <Meta name="keywords" content="十年生死 南山有壶酒 未定义人生 南山有壶酒的blog"></Meta>
+        <Meta name="viewport" content="width=device-width,initial-scale=1"></Meta>
+        <Meta name="description" content="欢迎来到我的世界"></Meta>
     </Head>
     <NuxtLayout name="toptitle">
         <template #default>
