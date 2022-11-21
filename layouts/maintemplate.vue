1adc07b7b280167d1d602b6c7200e609f4df717b
southernMD <2483723241@qq.com>
2022-11-21 20:56:09 +0800

    show navs

--- layouts/maintemplate.vue
+++ layouts/maintemplate.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="toptitle">
+    <div class="maintemplate">
 
         <Head>
-            <Link rel="stylesheet" href="//at.alicdn.com/t/c/font_3736505_bbzb36dxh2s.css">
+            <Link rel="stylesheet" href="//at.alicdn.com/t/c/font_3736505_2r0neje2dx6.css">
             </Link>
             <Link rel="icon" href="/favicon.ico">
             </Link>
@@ -13,40 +13,44 @@
                     <span>southernMD&nbsp;南山有壶酒</span>
                 </div>
                 <div class="nav-list">
-                    <div class="nav" v-for="val in navArr" :key="val" @click="go(val)">
+                    <div class="nav" v-for="(val,index) in navArr" :key="val" @click="go(val)" :class="{ active: val == '首页' }">
+                        <i class="iconfont" :class="navicons[index]"></i>
                         <span>{{ val }}</span>
                     </div>
-                </div>
-                <div class="switch">
-                    <ElSwitch v-model="value1" inline-prompt active-text="日" inactive-text="夜" />
                 </div>
                 <div class="menu" @click="showDrawer">
                     <i class="iconfont icon-caidan"></i>
                 </div>
                 <el-drawer v-model="drawerFlag" :append-to-body="true" :show-close="false" :with-header="false">
                     <div class="nav-list-phone">
-                        <div class="nav" v-for="val in navArr" :key="val" @click="go(val)">
-                            <span>{{ val }}</span>
+                        <div class="nav" v-for="(val,index) in navArr" :key="val" @click="go(val)"
+                            :class="{ active: val == '首页' }">
+                            <span><i class="iconfont" :class="navicons[index]"></i>{{ val }}</span>
                         </div>
                     </div>
                 </el-drawer>
             </div>
         </div>
         <slot></slot>
+        <div class="float-option">
+            <div class="one"></div>
+            <div class="two"></div>
+            <div class="three"></div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useRouter } from 'vue-router';
-import { ElSwitch, ElDrawer,ElButton } from 'element-plus'
+import { ElDrawer, ElButton } from 'element-plus'
 import { useApp } from '@/stores/index'
 const $router = useRouter()
 const AppPinia = useApp()
-const navArr = ['首页', '文章', '实验室', '平成十年']
-let value1 = ref(true)
+const navArr = ['首页', '文章', '留言版', '实验室', '十年','关于'];
+const navicons = ['icon-shouye','icon-wenzhang','icon-liuyan','icon-flask','icon-zhiwu','icon-guanyu']
 
-let drawerFlag = ref(true)
+let drawerFlag = ref(false)
 const go = (path: string) => {
     $router.push({
 
@@ -78,7 +82,7 @@ const showDrawer = () => {
 </script>
 
 <style scoped lang="less">
-.toptitle {
+.maintemplate {
     .title {
         width: 100vw;
         height: 55px;
@@ -100,7 +104,7 @@ const showDrawer = () => {
                 display: flex;
                 align-items: center;
                 padding-left: 10px;
-
+                cursor: pointer;
                 >span {
                     color: white;
                     font-size: 14px;
@@ -115,7 +119,7 @@ const showDrawer = () => {
                 .nav {
                     width: 80px;
                     height: 30px;
-                    margin-left: 5px;
+                    margin-left: 10px;
                     border-radius: .4em;
                     display: flex;
                     justify-content: center;
@@ -123,31 +127,34 @@ const showDrawer = () => {
                     cursor: pointer;
                     user-select: none;
 
-                    >span {
+                    >span,i{
                         font-size: 16px;
                         color: white;
                     }
-
+                    i{
+                        margin-right: 4px;
+                    }
                     &:hover {
                         background: white;
 
-                        >span {
+                        >span,i{
                             color: @title-color;
                         }
                     }
                 }
-            }
+                .active {
+                    background: white ;
 
-            @media (max-width:512px) {
-                .nav-list {
-                    display: none
+                    >span,i{
+                        color: @title-color ;
+                    }
                 }
             }
 
-            .switch {
-                position: absolute;
-                right: 0;
-                display: none;
+            @media (max-width:720px) {
+                .nav-list {
+                    display: none
+                }
             }
 
             .menu {
@@ -170,7 +177,7 @@ const showDrawer = () => {
                 }
             }
 
-            @media (max-width: 512px) {
+            @media (max-width: 720px) {
                 .menu {
                     display: block;
                 }
@@ -181,23 +188,76 @@ const showDrawer = () => {
             .title-main {
                 width: 100%;
             }
+
+            .title-main .nav-list {
+                position: absolute;
+                right: 10px;
+                top: 0;
+                bottom: 0;
+                margin: auto 0;
+            }
         }
     }
 }
 
-.nav-list-phone{
-    .nav{
+.nav-list-phone {
+    .nav {
         display: flex;
         align-items: center;
         height: 40px;
         user-select: none;
-        cursor: pointer;
-        >span{
+        z-index: 2;
+        position: relative;
+        transition: all .2s linear;
+        margin-top: 5px;
+
+        >span {
             padding-left: 20px;
+            z-index: 2;
+            position: relative;
+            cursor: pointer;
+            i{
+                margin-right: 5px;
+            }
         }
-        &:hover{
-            background-color: red;
+
+        &:not(.active):hover {
+            transform: translateY(-5px);
         }
+
+        &::after {
+            content: '';
+            cursor: pointer;
+            width: 90%;
+            height: 80%;
+            background-color: rgba(255, 255, 255, .5);
+            position: absolute;
+            left: 0;
+            right: 0;
+            margin: 0 auto;
+            z-index: 1;
+            border-radius: .5em;
+
+            &:hover {
+                background: red;
+            }
+        }
+    }
+
+    .active {
+        font-weight: bolder;
+    }
+}
+.float-option{
+    position: fixed;
+    z-index: 999;
+    bottom: 30px;
+    right: 30px;
+    >div{
+        width: 40px;
+        height: 40px;
+        margin-top: 10px;
+        background: red;
     }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,215 @@
 <template>
+
     <Head>
         <Title>南山有壶酒</Title>
-        <Meta name="author" content="southernMD"></Meta>
-        <Meta name="keywords" content="十年生死 南山有壶酒 未定义人生 南山有壶酒的blog"></Meta>
-        <Meta name="viewport" content="width=device-width,initial-scale=1"></Meta>
-        <Meta name="description" content="欢迎来到我的世界"></Meta>
+        <Meta name="author" content="southernMD">
+        </Meta>
+        <Meta name="keywords" content="南山有壶酒,未定义人生,南山有壶酒的blog">
+        </Meta>
+        <Meta name="viewport" content="width=device-width,initial-scale=1">
+        </Meta>
+        <Meta name="description" content="欢迎来到我的世界">
+        </Meta>
     </Head>
-    <NuxtLayout name="toptitle">
+    <NuxtLayout name="maintemplate">
         <template #default>
-            12445
+            <el-scrollbar>
+                <div style="height:100vh;">
+                    right: 20p;
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum nisi voluptas odio eligendi
+                    reprehenderit quos fugiat accusamus eveniet earum porro? Amet consectetur nesciunt quam vitae!
+                    Itaque repellat molestiae necessitatibus adipisci
+                </div>
+            </el-scrollbar>
+
         </template>
     </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-
+import { ElScrollbar } from 'element-plus';
 </script>
 
 <style lang="less">
-    .el-drawer__body {
-        padding: 0 !important;
-        background-color: @background-color;
+.el-drawer__body {
+    padding: 0 !important;
+    background-image: url(/tamaonsen.jpeg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position-x: -150px;
+
+    &::after {
+        content: '';
+        background: rgba(255, 255, 255, .3);
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
     }
-    body{
-        background-color: @background-color;
+}
+
+body {
+    &::before {
+        content: '';
+        display: block;
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        z-index: -2;
+        background: url(/tamaonsen.jpeg);
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
     }
+}
 </style>
