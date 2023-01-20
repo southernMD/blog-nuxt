bc3d04801c078de51a453392c617986b7611e68b
southernMD <2483723241@qq.com>
2023-01-20 20:04:43 +0800

    remove log

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,6 +35,16 @@ export default defineNuxtConfig({
             ElementPlus(),
         ],
         envDir: '~/env', // 指定env文件夹
+        build: {
+            minify: "terser",
+            terserOptions: {
+              compress: {
+                //生产环境时移除console
+                drop_console: true,
+                drop_debugger: true,
+              },
+            },
+        }
     },
     modules: ['@pinia/nuxt'],
 })
