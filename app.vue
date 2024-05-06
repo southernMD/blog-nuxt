ebf5981267a499a1cec8836f7340e0d72fc4109e
southernMD <2483723241@qq.com>
2024-05-06 17:23:10 +0800

    vercel speed-insights

--- app.vue
+++ app.vue
@@ -2,5 +2,10 @@
   <div>
     <NuxtLoadingIndicator /> 
     <NuxtPage></NuxtPage>
+    <SpeedInsights/>
   </div>
 </template>
+
+<script lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
